23aa052b10c8691217387905fd25c09374d478f7
SarahHei <sarahhei@compasseval.com>
2018-12-20 20:18:38 -0500

    User Updates

--- project2/models/user.js
+++ project2/models/user.js
@@ -102,7 +102,7 @@ module.exports = function(sequelize, DataTypes) {
 });
 
     User.associate =function(models) {
-      User.hasMany.(models.Forum, {
+      User.hasMany(models.Forum, {
         onDelete: "cascade"
       });
     };
